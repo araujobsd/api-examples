510cdf9050fcde453599e274e79306bfcbca7ac1
john.wang <john.wang@gandi.net>
2015-10-23 11:20:24 +0800

    count gandi sites and show all gandi site's info

---
+++ gandisitesample.py
@@ -0,0 +1,37 @@
+import xmlrpclib
+import sys
+
+# Connect to the API server
+api = xmlrpclib.ServerProxy('https://rpc.gandi.net/xmlrpc/')
+
+apikey = sys.argv[1]
+
+# Now you can call API methods.
+# You must authenticate yourself by passing
+# the API key as the first method's argument
+version = api.version.info(apikey)
+
+# Count your Gandi Sites
+
+GandiSitesNums = api.site.count(apikey)
+
+gandisite_count_msg = "You have %s Gandi Site" % GandiSitesNums
+if GandiSitesNums > 1:
+    gandisite_count_msg += "s"
+print gandisite_count_msg
+
+# Get all site id 
+siteid_list = []
+for eachsite in api.site.list(apikey):
+    for k,v in eachsite.items():
+        if k == "id":
+            siteid_list.append(v)
+print "Gandi Site IDs:", siteid_list
+
+#Get info on each Gandi site
+if len(siteid_list) > 0:
+    for siteid in siteid_list:
+        print api.site.info(apikey, siteid)
+        print 
+
+
