3add3b1b7dc2e6d6c4af115bdbcce8f6343a94bd
Marcelo Araujo <araujo@FreeBSD.org>
2015-12-02 11:44:22 +0800

    - Clean up and convert it to a module.

--- showgandicontactinfo.py
+++ showgandicontactinfo.py
@@ -1,35 +1,44 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+#  此範例程式碼用途為透過API金鑰，取得聯絡人相關資訊
 
-"""
-此範例程式碼用途為透過API金鑰，取得聯絡人相關資訊
-"""
+'''
+API INFO: http://doc.rpc.gandi.net/
+'''
 
 import xmlrpclib
-import sys
-from pprint import pprint
 
-#  連接API伺服器
-API = xmlrpclib.ServerProxy('https://rpc.gandi.net/xmlrpc/')
 
-if len(sys.argv) != 2:
-    print "%s %s" % (sys.argv[0], "<YOUR API KEY>")
-    exit(1)
+def show_contact_info(api_key):
+    '''
+        Receive the API_KEY and output the HANDLER and EMAIL.
+    '''
 
-APIKEY = sys.argv[1]
+    #  連接API伺服器
+    api = xmlrpclib.ServerProxy('https://rpc.gandi.net/xmlrpc/')
 
-#  您可以呼叫API方法
-#  您必須經由API金鑰作為第一個方法的參數進行驗證
-VERSION = API.version.info(APIKEY)
-print "gandi api version: " + VERSION['api_version']
+    #  您可以呼叫API方法
+    #  您必須經由API金鑰作為第一個方法的參數進行驗證
+    version = api.version.info(api_key)['api_version']
+    print "Gandi api version: %s" % (version)
 
-#  取得帳號ID
-HANDLE_LIST = []
-for eachcontact in API.contact.list(APIKEY):
-    HANDLE_LIST.append(eachcontact['handle'])
+    #  取得帳號ID
+    handle_list = []
+    for eachcontact in api.contact.list(api_key):
+        handle_list.append(eachcontact['handle'])
 
-print "Gandi Site IDs:", HANDLE_LIST
+    print "Gandi Site IDs: %s" % (handle_list)
 
-#  取得個別的Gandi帳號資料
+    #  取得個別的Gandi帳號資料
+    if len(handle_list) > 0:
+        for item in api.contact.list(api_key):
+            print "Handler: %s \t\t Contact: %s" % (
+                item['handle'], item['email'])
 
-if len(HANDLE_LIST) > 0:
-    pprint(API.contact.list(APIKEY))
+if __name__ == '__main__':
+    API_KEY = raw_input("Enter <YOUR API KEY>: ")
+    if len(API_KEY) < 24:
+        print "<YOUR API KEY> must have 24 characters"
+    else:
+        show_contact_info(API_KEY)
